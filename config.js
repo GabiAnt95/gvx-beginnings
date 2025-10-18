fc7075190da4fa837c0974c5b6f97765cf9f39fd
GabiAnt95 <GabinoAntOrt95@gmail.com>
2025-10-18 16:06:22 +0200

    cal link

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.APP_CONFIG = {
   email: "gabinoantort95@gmail.com",
   phone: "+34 664 657 037",
   location: "Madrid, Spain",
-  linkedin: "https://www.linkedin.com/in/gabino-antuna-ortiz/"
+  linkedin: "https://www.linkedin.com/in/gabino-antuna-ortiz/",
+  cal: "https://cal.com/gabiant-galtvx"
 };
-
